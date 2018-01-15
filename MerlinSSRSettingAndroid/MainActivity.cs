645b0a61744b3881c5e45ceb1426ddd9d8f6aa63
ZhenHong Xu <cecaa1030@gmail.com>
2018-01-15 17:07:47 +0800

    update

--- MerlinSSRSettingAndroid/MainActivity.cs
+++ MerlinSSRSettingAndroid/MainActivity.cs
@@ -1,6 +1,9 @@
-﻿using Android.App;
+﻿
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
+
 
 namespace MerlinSSRSettingAndroid
 {
@@ -21,6 +24,21 @@ namespace MerlinSSRSettingAndroid
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            LinearLayout serverListView = FindViewById<LinearLayout>(Resource.Id.serverListView);
+            
+            LayoutInflater inflater = LayoutInflater.From(serverListView.Context);
+            var entityView = inflater.Inflate(Resource.Layout.ServerEntity, null);            
+            var entityServerName = entityView.FindViewById<TextView>(Resource.Id.textServerName);
+            entityServerName.Text = "XXXX1";
+
+            serverListView.AddView(entityView);
+            
+            entityView = inflater.Inflate(Resource.Layout.ServerEntity, null);
+            entityServerName = entityView.FindViewById<TextView>(Resource.Id.textServerName);
+            entityServerName.Text = "XXXX2";
+
+            serverListView.AddView(entityView);
         }
     }
 }
--- MerlinSSRSettingAndroid/Resources/Resource.Designer.cs
+++ MerlinSSRSettingAndroid/Resources/Resource.Designer.cs
@@ -47,6 +47,18 @@ namespace MerlinSSRSettingAndroid
 			// aapt resource value: 0x7f050000
 			public const int myButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int serverListView = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int textServerAddress = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int textServerName = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int textServerPort = 2131034116;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -62,6 +74,9 @@ namespace MerlinSSRSettingAndroid
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int ServerEntity = 2130903041;
 			
 			static Layout()
 			{
